cb94e3a095d64b6269d0f4fb44d77dde77fb5d9c
FZ-c <fz-c@outlook.com>
2021-12-29 15:13:05 +0800

    Revert "增加进度条模式"
    This reverts commit 928780f4c4259a72ba927a57c4d75459bfc5ed9b.

--- LocalEvaluationTool/main.cpp
+++ LocalEvaluationTool/main.cpp
@@ -4,21 +4,8 @@
 
 using namespace std;
 
-void windowSet()
-{
-	//设置窗口大小
-	system("mode con cols=130 lines=30");
-	//禁用最大化按钮
-	HWND hWnd = GetConsoleWindow();
-	HMENU hMenu = GetSystemMenu(hWnd, false);
-	RemoveMenu(hMenu, SC_MAXIMIZE, MF_BYCOMMAND);
-	DestroyMenu(hMenu);
-	ReleaseDC(hWnd, NULL);
-}
-
 int main()
 {
-	windowSet();
 	std::string cmd;
 	while(1)
 	{
