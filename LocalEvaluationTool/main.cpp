4cd6dfb5846dd4ba2609968258a1d84bc90908f4
FZ-c <fz-c@outlook.com>
2021-12-29 15:13:57 +0800

    修改窗口设置

--- LocalEvaluationTool/main.cpp
+++ LocalEvaluationTool/main.cpp
@@ -4,10 +4,22 @@
 
 using namespace std;
 
+void windowSet()
+{
+	//设置窗口大小
+	system("mode con cols=130 lines=30");
+	//禁用最大化按钮
+	HWND hWnd = GetConsoleWindow();
+	HMENU hMenu = GetSystemMenu(hWnd, false);
+	RemoveMenu(hMenu, SC_MAXIMIZE, MF_BYCOMMAND);
+	DestroyMenu(hMenu);
+	ReleaseDC(hWnd, NULL);
+}
+
 int main()
 {
 	std::string cmd;
-	while(1)
+	while (1)
 	{
 		std::cout << CCommand::instance()->getExePath() << ">";
 		getline(cin, cmd);
